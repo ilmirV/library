691cca5e774a3c6aa4407e8908b39722282bc5d9
Raios Kristi <raioskristi@gmail.com>
2023-02-01 15:32:44 +0500

    Updates settings.py: updates SECRET_KEY and DEBUG

--- library/settings.py
+++ library/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,10 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nkwt3n$2+j0!^$qvs%s)mk)fxq&k#@@!2%bgm2=6_(83asr1j4'
+# SECRET_KEY = 'django-insecure-nkwt3n$2+j0!^$qvs%s)mk)fxq&k#@@!2%bgm2=6_(83asr1j4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nkwt3n$2+j0!^$qvs%s)mk)fxq&k#@@!2%bgm2=6_(83asr1j4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'True'))
 
 ALLOWED_HOSTS = ['77.223.98.223', 'localhost']
 
